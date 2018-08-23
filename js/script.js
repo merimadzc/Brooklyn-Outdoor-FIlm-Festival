abf3279371819eb624b1dcdd158a07abd80957b2
Merima Dzinovic <merimadzc@gmail.com>
2018-08-23 11:35:41 +0200

    Brooklyn Outdoor Film Festival Website

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* Full Screen Menu */
+
+$('.navToggle').click(function(){
+  $('.menu').toggleClass('menuOn');
+  $('nav').toggleClass('navOn');
+})
+
+/* Movies Trailer Modal */
+
+$(document).ready(function() {
+  var $videoSrc;  
+  $('.video-btn').click(function() {
+      $videoSrc = $(this).data( "src" );
+  });
+  console.log($videoSrc);
+
+  $('#myModal').on('shown.bs.modal', function (e) {
+    
+  $("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" ); 
+  })
+
+  $('#myModal').on('hide.bs.modal', function (e) {
+    $("#video").attr('src',$videoSrc); 
+  })   
+});
+
+
