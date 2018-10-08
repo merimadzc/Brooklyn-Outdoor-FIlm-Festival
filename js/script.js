85fe84531c1551b6b4330b0cdbef460cc8105d0a
Merima Dzinovic <merimadzc@gmail.com>
2018-10-08 17:37:03 +0200

    Add changes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,20 @@
 /* Full Screen Menu */
-
 $('.navToggle').click(function(){
   $('.menu').toggleClass('menuOn');
   $('nav').toggleClass('navOn');
 })
 
 /* Movies Trailer Modal */
-
 $(document).ready(function() {
   var $videoSrc;  
   $('.video-btn').click(function() {
       $videoSrc = $(this).data( "src" );
   });
   console.log($videoSrc);
-
-  $('#myModal').on('shown.bs.modal', function (e) {
-    
+  $('#myModal').on('shown.bs.modal', function (e) {    
   $("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" ); 
   })
-
   $('#myModal').on('hide.bs.modal', function (e) {
     $("#video").attr('src',$videoSrc); 
   })   
 });
-
-
